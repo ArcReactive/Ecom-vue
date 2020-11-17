0edcb086ebbdf2609e2378e9bbd6c7871346bf56
NuwanSudusinghe <npsudusinghe@gmail.com>
2020-11-17 20:56:48 +0530

    Added about us page

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,7 +1,64 @@
 <template>
-  <div class="home">
-    <Navbar></Navbar>
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+    <div class="hero">
+      <Navbar></Navbar>
+        <div class="container h-100">
+          <div class="row h-100 justify-content-center align-items-center">
+            <div class="col-md-5">
+              <div class="hero-content">
+                <h1 class="hero-title">
+                  About Us
+                </h1>
+                <p>
+                  One brand, many companies, and many, many people – that’s us in a nutshell. Spread all over the world, we have a passion for home furnishing and an inspiring shared vision: to create a better everyday life for the many people. This, together with our straightforward business idea, shared values, and a culture based on the spirit of togetherness, guides us in everything we do.
+                </p>
+              </div>
+            </div>
+            <div class="col-md-7">
+                <div class="graphic">
+                    <img src="../assets/img/svg/world-map_2400.jpg" alt="" width="450px">
+                </div>
+            </div>
+          </div>
+        </div>
+        <div class="container h-100">
+          <div class="row h-100 justify-content-center align-items-center">
+            <div class="col-md-5">
+                <div class="graphic">
+                    <img src="../assets/img/svg/woman-carrying-IKEA-PS-watering-cans-ai020102-PH126968-ikea-l-thumb.jpg" alt="" width="450px">
+                </div>
+            </div>
+            <div class="col-md-7">
+              <div class="hero-content">
+                <h1 class="hero-title">
+                  Together, all over the world
+                </h1>
+                <p>
+                  The IKEA Brand unites thousands of co-workers and hundreds of companies with different owners around the world. The retail business is organised in a franchise system with 12 franchisees in 54 markets that – together with the franchisor and other parts of the business – continuously improve and develop the brand.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+    </div>
 </template>
+
+<script>
+export default {
+  name: "AboutUs",
+  props: {
+    msg: String
+  }
+};
+</script>
+
+<style scoped lang="scss">
+  .hero{
+    padding-top: 7rem;
+    width: 100%;
+    height: 500px;
+    text-align: left;
+  }
+
+
+
+</style>
